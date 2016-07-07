7506191789dab36b81c27078dfe689f0d171762a
Kurt Foster <kurt@salsadigital.com.au>
2016-07-07 12:24:36 +1000

    ARTS-433 - created a link around the simple promotional image

--- doca_theme/templates/promotional-feature/paragraphs-item--quick-item--promotional-feature.tpl.php
+++ doca_theme/templates/promotional-feature/paragraphs-item--quick-item--promotional-feature.tpl.php
@@ -5,7 +5,7 @@
  */
 ?>
 <div class="promotional-feature__left">
-  <?php print render($content['field_pbundle_image']); ?>
+  <a href="<?php print $content['field_pbundle_destination']['#items'][0]['url']; ?>" title="<?php print $content['field_pbundle_title']['#items'][0]['safe_value']; ?>"><?php print render($content['field_pbundle_image']); ?></a>
 </div>
 
 <div class="promotional-feature__right">
