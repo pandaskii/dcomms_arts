472d1aee58b7151905810070beccbdb856096e28
solange kershaw <solange@salsadigital.com.au>
2016-08-04 18:13:58 +1000

    Fixes for: - ARTS-466, progress bar padding; - ARTS-454: sidebar img size - ARTS-441: icon size.

--- doca_theme/templates/spotlight/paragraphs-item--spotlight-alt.tpl.php
+++ doca_theme/templates/spotlight/paragraphs-item--spotlight-alt.tpl.php
@@ -4,7 +4,7 @@
  * Channels theme implementation for a container around three column items.
  */
 ?>
-<div class="palette__pale-blue">
+<div class="palette__pale-blue spotlight--alt">
   <div class="layout-three-column spacer--medium">
     <?php print render($content); ?>
   </div>
