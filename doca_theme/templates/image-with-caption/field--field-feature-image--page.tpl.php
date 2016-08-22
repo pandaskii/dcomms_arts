f9703cfd841040538145b459e62282bf030a1005
solange kershaw <solange@salsadigital.com.au>
2016-08-22 16:12:04 +1000

    ARTS-454: Img attribution tweaks ARTS-447: tweak icon heights.

--- doca_theme/templates/image-with-caption/field--field-feature-image--page.tpl.php
+++ doca_theme/templates/image-with-caption/field--field-feature-image--page.tpl.php
@@ -14,8 +14,8 @@
   <?php foreach ($items as $delta => $item): ?>
     <?php print render($item); ?>
     <div class="image-with-caption__caption">
+      <?php print render($item['#item']['field_artist'][LANGUAGE_NONE][0]['safe_value']).' '; ?>
       <?php print render($item['#item']['field_file_image_title_text'][LANGUAGE_NONE][0]['safe_value']); ?>
-      <?php print render($item['#item']['field_artist'][LANGUAGE_NONE][0]['safe_value']); ?>
     </div>
   <?php endforeach; ?>
 </div>
