d62fef908213439f3b8c55565c6bcfb75e1cdad8
Kurt Foster <kurt@salsadigital.com.au>
2016-08-22 20:46:55 +1000

    ARTS-484 - further updates to the display of updates on Funding content type

--- doca_theme/templates/update/paragraphs-item--funding-update.tpl.php
+++ doca_theme/templates/update/paragraphs-item--funding-update.tpl.php
@@ -5,7 +5,7 @@
  */
 ?>
 <div class="update">
-  <div class="update__icon"><?php print render($content['field_update_type_term']); ?></div>
+  <div class="update__icon"><?php print render($content['field_update_type']); ?></div>
   <div class="update__title"><?php print render($content['field_pbundle_destination']); ?></div>
   <div class="update__date"><?php print render($content['field_update_date']); ?></div>
   <div class="update__summary"><?php print render($content['field_update_summary_text']); ?></div>
