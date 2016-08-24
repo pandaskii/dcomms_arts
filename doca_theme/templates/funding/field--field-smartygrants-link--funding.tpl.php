5cc07c70afb9ada4e5acb218b4f3a053b7be8c41
Kurt Foster <kurt@salsadigital.com.au>
2016-08-24 15:24:52 +1000

    ARTS-363 - there was an extra div on the smarty grants link for funding

--- doca_theme/templates/funding/field--field-smartygrants-link--funding.tpl.php
+++ doca_theme/templates/funding/field--field-smartygrants-link--funding.tpl.php
@@ -7,4 +7,3 @@
 <div class="spacer--bottom-large">
   <a class="link-external__no-icon" target="_blank" href="<?php print $items[0]['#element']['url']; ?>"><button class="button-primary form-submit"><?php print t('Apply Now'); ?></button></a>
 </div>
-</div>
