1b270a8c4f4bce0116f404b9be7678873da1164e
Kurt Foster <kurt@salsadigital.com.au>
2016-08-26 14:14:54 +1000

    Removing a php notice

--- doca_theme/templates/promotional-feature/paragraphs-item--quick-item--promotional-feature.tpl.php
+++ doca_theme/templates/promotional-feature/paragraphs-item--quick-item--promotional-feature.tpl.php
@@ -3,10 +3,11 @@
  * @file
  * Channels theme implementation for a single paragraph item.
  */
+$bundle_title = isset($content['field_pbundle_title']) ? $content['field_pbundle_title']['#items'][0]['safe_value'] : '';
 ?>
 <div class="promotional-feature__left">
   <a href="<?php print $content['field_pbundle_destination']['#items'][0]['url']; ?>"
-     title="<?php print $content['field_pbundle_title']['#items'][0]['safe_value']; ?>"><?php print render($content['field_pbundle_image']); ?></a>
+     title="<?php print $bundle_title; ?>"><?php print render($content['field_pbundle_image']); ?></a>
 </div>
 
 <div class="promotional-feature__right">
