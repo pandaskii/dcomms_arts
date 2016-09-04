3ca5365500493855714545f90062a22b67092fbb
Kurt Foster <kurt@salsadigital.com.au>
2016-09-05 08:57:16 +1000

    ARTS-400 - updating the GA Tag manager code

--- doca_theme/templates/html.tpl.php
+++ doca_theme/templates/html.tpl.php
@@ -64,22 +64,14 @@
      tabindex="1"><?php print $skip_link_text; ?></a>
 <?php endif; ?>
 <!-- Google Tag Manager -->
-<noscript>
-  <iframe src="//www.googletagmanager.com/ns.html?id=GTM-WCC3TZ"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
-<script>(function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({
-      'gtm.start': new Date().getTime(), event: 'gtm.js'
-    });
-    var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src =
-      '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-  })(window, document, 'script', 'IRXDataLayer', 'GTM-WCC3TZ');</script>
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TRVB2N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){wl=wl||[];wl.push(
+{'gtm.start': new Date().getTime(),event:'gtm.js'}
+);var f=d.getElementsByTagName(s)0,
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TRVB2N');</script>
 <!-- End Google Tag Manager -->
 <?php print $page_top; ?>
 <?php print $page; ?>
