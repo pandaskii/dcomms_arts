8330eaae6d9fdeb9e14ec14f95c1a7575445145b
Kurt Foster <kurt@salsadigital.com.au>
2016-09-06 16:13:00 +1000

    Another update to the GTM code for Nicholas Keilar

--- doca_theme/templates/html.tpl.php
+++ doca_theme/templates/html.tpl.php
@@ -70,7 +70,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-58Z5ZP');</script>
+})(window,document,'script','IRXDataLayer','GTM-58Z5ZP');</script>
 <!-- End Google Tag Manager -->
 <?php print $page_top; ?>
 <?php print $page; ?>
